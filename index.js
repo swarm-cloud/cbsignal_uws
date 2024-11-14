c7d5da9f6e6f2d8b0f1e6912e1e3fb999f559f43
snowinszu <86755838@qq.com>
2024-11-14 18:32:12 +0800

    signal compact

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ function masterProc() {
     logger.warn(`master ${process.pid} is running, numCPUs ${numCPUs}`);
     if (process.env.WORKERS) {
         numCPUs = Number(process.env.WORKERS);
-    } else if (numCPUs === 2) {
-        numCPUs = 1;
+    } else {
+        numCPUs--;
     }
     if (numCPUs <= 1 || !configObject.redis) {
         // if (true) {
